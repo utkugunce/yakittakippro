1b32daf172693b56af813147122a76ad9f5693c1
Utku Günçe <utkugunce@gmail.com>
2026-01-12 12:34:18 +0300

    fix: Add declarations.d.ts to resolve Leaflet image import errors

---
+++ declarations.d.ts
@@ -0,0 +1,9 @@
+declare module 'leaflet/dist/images/marker-icon.png' {
+    const value: string;
+    export default value;
+}
+
+declare module 'leaflet/dist/images/marker-shadow.png' {
+    const value: string;
+    export default value;
+}
